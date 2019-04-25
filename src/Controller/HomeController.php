aaf0665ed42ab9e0e28e16688d2c8c7b204b1cf5
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-04-25 18:55:58 +0200

    wip

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/page-accueil", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+    * @Route("/contact", name="contact_index")
+    */
+    public function contact()
+    {
+        return $this->json(["contact"]);
+    }
+}
