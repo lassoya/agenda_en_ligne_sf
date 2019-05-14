da3bc2402b702ff13f59193c55d833d83949ef1c
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-05-14 20:12:37 +0200

    wip

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,5 +2,6 @@ import $ from 'jquery';
 
 $('#add').click(() => {
   const firstInput = $('#container-number input').first().clone();
+  firstInput.val('');
   $('#container-number').append(firstInput);
 });
