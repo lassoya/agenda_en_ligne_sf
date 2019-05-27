d6f6f981b55a14ac92797418d921ac0418e23835
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-05-27 18:42:44 +0200

    wip

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -22,16 +22,21 @@ class PhoneController extends AbstractController
     }
 
     /**
+     * @Route("/phone/{idContact}/{phone}", name="phone_edit")
      * @Route("/phone/{idContact}", name="phone_add")
      */
-    public function edit(EntityManagerInterface $em, Request $request, Contact $idContact) {
+    public function edit(EntityManagerInterface $em, Request $request, Contact $idContact, Phone $phone = null) {
+      if(!$phone) {
+        $phone = new Phone();
+        $em->persist($phone);
+      }
+
       if('POST'=== $request->getMethod() &&
         $request->request->has('number')) {
           $number = $request->request->get('number');
-          $phone = new Phone();
           $phone->setNumber($number);
           $phone->setContact($idContact);
-          $em->persist($phone);
+
           $em->flush();
 
           return $this->redirectToRoute('contact2_edit', [
@@ -39,7 +44,9 @@ class PhoneController extends AbstractController
           ]);
       }
 
-      return $this->render('phone/edit.html.twig');
+      return $this->render('phone/edit.html.twig', [
+        'phone' => $phone
+      ]);
     }
 
     /**
